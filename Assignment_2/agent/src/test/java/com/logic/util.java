d0daec05f5b6271764b1737fd5bc949ffc358fb3
MarcusRLE <s230985@dtu.dk>
2025-05-03 22:17:27 +0200

    First batch of resolution tests

--- Assignment_2/agent/src/test/java/com/logic/util.java
+++ Assignment_2/agent/src/test/java/com/logic/util.java
@@ -1,5 +1,8 @@
 package com.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class util {
 
     public static String falseEquality(Expression exp1, Expression exp2) {
@@ -8,5 +11,23 @@ public class util {
         result += "\nGot:\n";
         result += exp1.CNF().toString(false) + " compared to " + exp2.CNF().toString(false);
         return result;
+    }
+
+    public static boolean sameContent(List<Expression> list1, List<Expression> list2) {
+        if (list1.size() != list2.size()) {
+            return false;
+        }
+
+        boolean sameContent = false;
+        List<Expression> list2Copy = new ArrayList<>(list2);
+        for (Expression exp1 : list1) {
+            sameContent = false;
+            if(list2Copy.contains(exp1)) {
+                list2Copy.remove(exp1);
+                sameContent = true;
+            }
+        }
+
+        return sameContent;
     }
 }
