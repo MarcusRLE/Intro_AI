51f9b82e2aa6f4735c03b9ed5fdfdc27cfa35e63
maximillian <maxwork.bm@gmail.com>
2025-05-04 19:05:31 +0200

    Add initial unit tests for contraction and revision methods in AmgTest

---
+++ Assignment_2/agent/src/test/java/com/logic/AmgTest.java
@@ -0,0 +1,56 @@
+package com.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class AmgTest {
+    
+    @Test
+    public void ContractionSuccess() {
+
+    }
+
+    @Test 
+    public void RevisionSuccess() {
+
+    }
+
+    @Test
+    public void ContractionInclusion() {
+
+    }
+
+    @Test 
+    public void RevisionInclusion() {
+
+    }
+
+    @Test
+    public void ContractionVacuity() {
+
+    }
+
+    @Test
+    public void RevisionVacuity() {
+
+    }
+
+    @Test
+    public void RevisionConsistency() {
+
+    }
+
+    @Test
+    public void ContractionExtensionality() {
+
+    }
+
+    @Test
+    public void RevisionExtensionality() {
+
+    }
+}
